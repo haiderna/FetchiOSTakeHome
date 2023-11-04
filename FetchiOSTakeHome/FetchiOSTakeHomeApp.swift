133cb892018a2b9ed4f19f168c62760b68f115b1
Najia <haiderna@vcu.edu>
2023-11-04 18:44:05 -0400

    Initial Commit

---
+++ FetchiOSTakeHome/FetchiOSTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchiOSTakeHomeApp.swift
+//  FetchiOSTakeHome
+//
+//  Created by Najia Haider on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchiOSTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
